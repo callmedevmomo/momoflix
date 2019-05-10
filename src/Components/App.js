ccb508efe697e4c6495ce6102b7d93afe93a93b4
momo <callmedevmomo@gmail.com>
2019-05-10 17:53:31 +0900

    CSS in React with styled-components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Router from "Components/Router";
-import Header from "Components/Header";
 function App() {
   return (
     <>
-      <Header />
       <Router />
     </>
   );
